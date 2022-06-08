26f97e48f27d04b43e4b6376e59790d35ce4165b
Kevin Koshy <kevin_koshy@hotmail.com>
2022-06-08 18:13:47 +0530

    General Formatting

--- app.js
+++ app.js
@@ -10,49 +10,48 @@ const saltRounds = 10;
 
 app.use(express.static("public"));
 app.set("view engine", "ejs");
-app.use(bodyParser.urlencoded({extended:true}));
-
+app.use(bodyParser.urlencoded({extended: true}));
 
 
 mongoose.connect("mongodb://localhost:27017/userDB");
 const userSchema = new mongoose.Schema({
-    email:String,
-    password:String
+    email: String,
+    password: String
 });
 
 
 const User = new mongoose.model("User", userSchema);
 
 
-app.listen(3000, function(){
+app.listen(3000, function () {
     console.log("Server started on port 3000");
 });
 
-app.get("/", function(req, res){
+app.get("/", function (req, res) {
     res.render("home");
 });
 
 
-app.get("/login", function(req, res){
+app.get("/login", function (req, res) {
     res.render("login");
 });
 
-app.get("/register", function(req, res){
+app.get("/register", function (req, res) {
     res.render("register");
 });
 
-app.post("/login", function(req, res){
+app.post("/login", function (req, res) {
     const userName = req.body.username;
     const password = req.body.password;
 
 
-    User.findOne({email:userName}, function (err, foundUser){
+    User.findOne({email: userName}, function (err, foundUser) {
         if (err) {
             console.log(err);
         } else {
-            if(foundUser){
-                bcrypt.compare(password, foundUser.password, function(err, result){
-                    if(result === true){
+            if (foundUser) {
+                bcrypt.compare(password, foundUser.password, function (err, result) {
+                    if (result === true) {
                         res.render("secrets")
                     }
                 })
@@ -61,21 +60,18 @@ app.post("/login", function(req, res){
     })
 });
 
-app.post("/register", function(req, res){
-    bcrypt.hash(req.body.password, saltRounds, function(err, hash){
-    const newUser = new User({
-        email:req.body.username,
-        password:hash
-    });
-        newUser.save(function(err){
-        if(err){
-            console.log(err);
-        }
-        else{
-            res.render("secrets");
-        }
-    });
+app.post("/register", function (req, res) {
+    bcrypt.hash(req.body.password, saltRounds, function (err, hash) {
+        const newUser = new User({
+            email: req.body.username, password: hash
+        });
+        newUser.save(function (err) {
+            if (err) {
+                console.log(err);
+            } else {
+                res.render("secrets");
+            }
+        });
 
     });
-
 });
